3d14a376836fcd1fe2012bc7362554b97f08dc5f
Nhr98 <nirinasoa.herilanto@gmail.com>
2021-02-26 17:05:50 +0300

    evenment onclick

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,21 @@
+import PropTypes from 'prop-types'
+
+
 // tsy maintsy respectena ny rang anle color sy text ao am header.js sy Button.js
-const Button = ({color, text}) => {
-    return  <button style={{backgroundColor: color,}} className="btn">{color, text}</button>
+const Button = ({color, text, onClick}) => {
+    return  (
+        <button onClick={onClick} style={{backgroundColor: color,}} className="btn">{color, text}</button>
+    )
+}
+
+Button.defaultProps = {
+    color: 'steelblue'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
 }
 
 export default Button
