6e17f0985f1a668aee3275c84f2c2d8571f6f71c
Nhr98 <nirinasoa.herilanto@gmail.com>
2021-02-28 15:32:20 +0300

    creation du form task

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,8 +1,23 @@
 const AddTask = () => {
     return (
-        <div>
-            
-        </div>
+        <form className='add-form'>
+            <div className='form-control'>
+                <lalel>Task</lalel>
+                <input type='text' placeholder='Add' />
+            </div>
+
+            <div className='form-control'>
+                <lalel>Day/time</lalel>
+                <input type='text' placeholder='Day/time' />
+            </div>
+
+            <div className='form-control'>
+                <lalel>Set reminder</lalel>
+                <input type='checkbox' />
+            </div>
+            <input type='submit' value='Save'/>
+
+        </form>
     )
 }
 
