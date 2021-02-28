29d25fcee6c94f051cd5b7700593c351fd287c86
Nhr98 <nirinasoa.herilanto@gmail.com>
2021-02-28 16:31:03 +0300

    modif title: show add

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({title, onAdd}) => {
+const Header = ({title, onAdd, showAdd}) => {
     return (
         <header className="header">
             <h1 className="logo">{title}</h1>
-            <Button color='green' text="Ajouter" onClick={onAdd}/>
+            <Button color='green' text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
         </header>
     )
 }
