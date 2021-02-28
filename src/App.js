95b5b59ec898a16f1ac28908c246a3f4e50374ef
Nhr98 <nirinasoa.herilanto@gmail.com>
2021-02-28 17:36:39 +0300

    manque d'information sur

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState } from 'react'
+import {BroswerRouter as Router, Route} from 'react-router-dom'
 import Header from '../src/components/Header'
+import Footer from '../src/components/Footer'
 import Tasks from '../src/components/Tasks'
 import AddTask from '../src/components/AddTask'
+import About from '../src/components/About'
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -44,13 +47,16 @@ const App = () => {
   }
 
   return (
+    <Router>
     <div className="container">
       <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No tasks to show here !'}
+      <Route path='/about' component={About}/>
+      <Footer />
     </div>
+    </Router>
   );
 }
-
 
 export default App;
